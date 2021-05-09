9f3051c0d74378f3c0b4f77e131082b8920c4a7b
Andrei Kniazev <worldpwn@hotmail.com>
2021-05-09 11:00:50 +0200

    Ref content now will pick doc name if custom text is null

--- src/DialogReferenceContent.js
+++ src/DialogReferenceContent.js
@@ -4,9 +4,18 @@ export default class ContentDataReference {
     * @param {string | null} text
     * @param {string | null} documentId
     */
-    constructor(id, text, documentId) {
+    constructor(id, text, documentId, documentName) {
         this.id = id;
-        this.text = text;
+        this._text = text;
         this.documentId = documentId;
+        this.documentName = documentName
     }
+
+    /**
+     * Will use document name if text is null.
+     */
+    get text() {
+        return this._text == null ? this.documentName : this._text;
+    }
+
 }
--- src/DialogReferenceContent.spec.js
+++ src/DialogReferenceContent.spec.js
@@ -6,13 +6,33 @@ describe("DialogReferenceContent.ctor", () => {
         const id = "8C3D3BCD-BCE3-49C4-8F52-9586FE1A31B9";
         const text = "value";
         const documentId = "1C3D3BCD-BCE3-49C4-8F52-9586FE1A31B9";
+        const docName = "Oliver";
 
         // act
-        const content = new DialogReferenceContent(id, text, documentId);
+        const content = new DialogReferenceContent(id, text, documentId, docName);
 
         // assert
         expect(content.id).toBe(id);
         expect(content.text).toBe(text);
         expect(content.documentId).toBe(documentId);
+        expect(content.documentName).toBe(docName);
     });
+
+});
+
+describe("DialogReferenceContent.text", () => {
+    it("text is null, should return docname", () => {
+        // arrange
+        const id = "8C3D3BCD-BCE3-49C4-8F52-9586FE1A31B9";
+        const text = null;
+        const documentId = "1C3D3BCD-BCE3-49C4-8F52-9586FE1A31B9";
+        const docName = "Oliver";
+
+        // act
+        const content = new DialogReferenceContent(id, text, documentId, docName);
+
+        // assert
+        expect(content.text).toBe(docName);
+    });
+
 });
