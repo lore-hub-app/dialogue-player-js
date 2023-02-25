884d690a18bf47f77300b143849fdd483124ac6a
Andrey Kniazev <worldpwn@hotmail.com>
2023-02-25 11:22:36 +0100

    Add i18n plugin

--- packages/embedded-player/src/App.vue
+++ packages/embedded-player/src/App.vue
@@ -7,5 +7,6 @@
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
+
--- packages/embedded-player/src/plugins/index.ts
+++ packages/embedded-player/src/plugins/index.ts
@@ -7,11 +7,12 @@
 // Plugins
 import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
-
+import { i18n } from './i18n'
 // Types
 import type { App } from 'vue'
 
-export function registerPlugins (app: App) {
+export function registerPlugins(app: App) {
   loadFonts()
   app.use(vuetify)
+  app.use(i18n);
 }
