a963587cfce475d895c1c75802b53e6d2ba536c7
Andrey Kniazev <worldpwn@hotmail.com>
2023-04-15 21:34:15 +0200

    remove not needed browsers

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './.',
-  /* Run tests in files in parallel */
   fullyParallel: true,
   forbidOnly: true,
   retries: 2,
@@ -21,20 +14,10 @@ export default defineConfig({
     baseURL: 'http://localhost:5174',
     trace: 'on-first-retry',
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    }
   ],
 });
